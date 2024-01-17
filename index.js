0ee15af4efcd8d7c3f52b45eec56947f504752e1
SergEnergy <kis8883244@gmail.com>
2024-01-17 14:35:51 +0300

    add: add article

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+window.addEventListener('load', mainFunction);
+function mainFunction() {
+  const headerLinkList = document.querySelector('.navigation');
+
+  headerLinkList.addEventListener('click', (event) => {
+    const headerLinks = document.querySelectorAll('.navigation__link');
+    headerLinks.forEach((link) => {
+      link.classList.remove('text_emphasized');
+      if (event.target === link) {
+        console.log(link.classList.add('text_emphasized'));
+      }
+    });
+  });
+
+  console.log(headerLinks);
+}
+
+//sticky header
 window.addEventListener('scroll', getStickyStyles);
 
 function getStickyStyles() {
